dc1d789eede44298c226598de806db00a4386428
Aldo Matias Lazo Rocha <aldomatias.lazo@uvic.cat>
2022-11-18 17:31:33 +0100

    Implemented a loop for reading text from a client and encode the text to base64

--- app/src/test/java/cat/uvic/teknos/m09/remotecrypto/ServerTest.java
+++ app/src/test/java/cat/uvic/teknos/m09/remotecrypto/ServerTest.java
@@ -4,11 +4,8 @@
 package cat.uvic.teknos.m09.remotecrypto;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
+class ServerTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
