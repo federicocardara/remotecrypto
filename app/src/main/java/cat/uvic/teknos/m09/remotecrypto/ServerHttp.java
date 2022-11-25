5ef4a2f2a6a3912f455f8fc3068a24566c5bbe9d
karim <abdelkarim.el@uvic.cat>
2022-11-25 19:02:34 +0100

    build and repository cryptoutil

--- app/src/main/java/cat/uvic/teknos/m09/remotecrypto/ServerHttp.java
+++ app/src/main/java/cat/uvic/teknos/m09/remotecrypto/ServerHttp.java
@@ -1,36 +1,26 @@
 package cat.uvic.teknos.m09.remotecrypto;
 
+import cat.uvic.teknos.m09.remotecrypto.thread.ServerHttpThread;
 import rawhttp.core.RawHttp;
 import rawhttp.core.RawHttpRequest;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class ServerHttp {
     private static RawHttp http = new RawHttp();
     public static int PORT = 50002;
 
-    public ServerHttp() {
-        ThreadPoolExecutor pool = new ThreadPoolExecutor();
-
-
-        try{
-            run();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    private void run() throws IOException {
+    public ServerHttp() throws IOException {
+        ExecutorService pool = Executors.newFixedThreadPool(10);
         ServerSocket server = new ServerSocket(PORT);
 
         Socket client  = server.accept();
-        RawHttpRequest request = http.parseRequest(client.getInputStream());
-
-
-
+        pool.execute(new ServerHttpThread(client));
 
     }
 }
