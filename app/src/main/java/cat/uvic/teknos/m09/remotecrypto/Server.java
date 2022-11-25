aa1447633c87e9ed74853937b1fae47126a60809
Arnau Barra Garcia <arnau.barra@uvic.cat>
2022-11-25 15:36:49 +0100

    Added client class and exceptions

--- app/src/main/java/cat/uvic/teknos/m09/remotecrypto/Server.java
+++ app/src/main/java/cat/uvic/teknos/m09/remotecrypto/Server.java
@@ -5,10 +5,15 @@ package cat.uvic.teknos.m09.remotecrypto;
 
 import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Base64;
 
 public class Server {
     public static  final int PORT = 50001;
+    private static ServerSocket server;
+    private static BufferedReader inputStream;
+    private static PrintWriter outputStream;
+    private static Socket client;
     public static void main(String[] args) throws IOException {
         var server = new ServerSocket(PORT);
         System.out.println("Server listening on port " + server.getLocalPort());
---
+++ app/src/main/java/cat/uvic/teknos/m09/remotecrypto/exceptions/serverSocketException.java
@@ -0,0 +1,7 @@
+package cat.uvic.teknos.m09.remotecrypto.exceptions;
+
+public class serverSocketException {
+    public void ServerSocketException() {
+        System.out.println("ERROR: CAN'T CREATE SERVER SOCKET");
+    }
+}
