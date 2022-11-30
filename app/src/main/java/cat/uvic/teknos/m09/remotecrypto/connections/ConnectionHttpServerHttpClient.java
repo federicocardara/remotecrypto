1bfaa81b673d5e7e7ba54c717a3218adbb435206
Pol Sañé Jové <pol.sane@uvic.cat>
2022-11-30 20:44:17 +0100

    response rreturns an HTML page git add .git add .git add .git add .git add .git add .git add .git add .! 😀

--- app/src/main/java/cat/uvic/teknos/m09/remotecrypto/connections/ConnectionHttpServerHttpClient.java
+++ app/src/main/java/cat/uvic/teknos/m09/remotecrypto/connections/ConnectionHttpServerHttpClient.java
@@ -2,41 +2,57 @@ package cat.uvic.teknos.m09.remotecrypto.connections;
 
 import rawhttp.core.RawHttp;
 import rawhttp.core.RawHttpRequest;
+import rawhttp.core.RawHttpResponse;
 
 import java.io.*;
 import java.net.Socket;
 import java.util.Base64;
 
 public class ConnectionHttpServerHttpClient {
-
     private BufferedReader inputStream;
     private PrintWriter outputStream;
-    private final Base64.Encoder encoder = Base64.getEncoder();
+    private final Base64.Encoder encoder;
     private Socket client;
-    private RawHttp http = new RawHttp();
+    private RawHttp http;
 
     public ConnectionHttpServerHttpClient(Socket socket, BufferedReader inputStream, PrintWriter outputStream) {
         this.client = socket;
         this.inputStream = inputStream;
         this.outputStream = outputStream;
+        http=new RawHttp();
+        encoder = Base64.getEncoder();
     }
 
     public void connection(){
         try {
             RawHttpRequest request = http.parseRequest(client.getInputStream());
+            String body="<!DOCTYPE html>\n" +
+                    "<html>\n" +
+                    "<body>\n" +
+                    "\n" +
+                    "<h1>My First Heading</h1>\n" +
+                    "<p>My first paragraph.</p>\n" +
+                    "\n" +
+                    "</body>\n" +
+                    "</html>";
             System.out.println(request.getUri());
-
-            if (request.getUri().getPath().equals("/saysomething")) {
-                http.parseResponse("HTTP/1.1 200 OK\n" +
-                        "Content-Type: text/plain\n" +
-                        "Content-Length: 25\n" +
-                        "\n" +
-                        "en pol es molt impacient\n").writeTo(client.getOutputStream());
-            } else {
-                http.parseResponse("HTTP/1.1 404 Not Found\n" +
-                        "Content-Type: text/plain\n" +
-                        "Content-Length: 0\n" +
-                        "\n").writeTo(client.getOutputStream());
+            if(request.getMethod().equals("GET")) {
+                if (request.getUri().getPath().equals("/saysomething")) {
+                    http.parseResponse(
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: html\r\n" +
+                                    "Content-Length: " + body.length() + "\r\n" +
+                                    "Server: localhost\r\n" +
+                                    "\r\n" +
+                                    body).writeTo(client.getOutputStream());
+                } else {
+                    http.parseResponse("HTTP/1.1 404 Not Found\n" +
+                            "Content-Type: text/plain\n" +
+                            "Content-Length: 0\n" +
+                            "\n").writeTo(client.getOutputStream());
+                }
+            }else {
+                //idk
             }
         } catch (IOException e) {
             e.printStackTrace();
