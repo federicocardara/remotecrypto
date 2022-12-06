f0ddff994f521892de9e7fb5020732fbf16d3379
wojciech wydryszek <wojciech.wydryszek@uvic.cat>
2022-12-06 18:49:06 +0100

    add try/catch to control empty data

--- app/src/main/java/cat/uvic/teknos/m09/remotecrypto/connections/ConnectionHttpServerHttpClient.java
+++ app/src/main/java/cat/uvic/teknos/m09/remotecrypto/connections/ConnectionHttpServerHttpClient.java
@@ -1,14 +1,12 @@
 package cat.uvic.teknos.m09.remotecrypto.connections;
 
 import cat.uvic.teknos.m09.polsane.cryptoutils.CryptoUtils;
-import cat.uvic.teknos.m09.remotecrypto.exceptions.RemoteCryptoArrayException;
 import rawhttp.core.RawHttp;
 import rawhttp.core.RawHttpRequest;
 
 import java.io.*;
 import java.net.Socket;
 import java.util.Base64;
-import java.util.Properties;
 
 public class ConnectionHttpServerHttpClient {
     private final Base64.Encoder encoder;
@@ -17,7 +15,7 @@ public class ConnectionHttpServerHttpClient {
 
     public ConnectionHttpServerHttpClient(Socket socket) {
         this.client = socket;
-        http=new RawHttp();
+        http = new RawHttp();
         encoder = Base64.getEncoder();
     }
 
@@ -26,49 +24,85 @@ public class ConnectionHttpServerHttpClient {
             RawHttpRequest request = http.parseRequest(client.getInputStream());
             String queryStr= request.getUri().getQuery();
             String[] query = queryStr.split("=");
-            String data=query[1];
+            String data;
 
-            var hashData = CryptoUtils.hash(data.getBytes()).getHash();
+            try {
+                data=query[1];
+                System.out.println("si");
 
-            String hash = new String(hashData);
 
-            String body="<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<body>\n" +
-                    "\n" +
-                    "<h1>Text to Base64</h1>\n" +
-                    "<p>"+hash+"</p>\n" +
-                    "\n" +
-                    "</body>\n" +
-                    "</html>";
+                var hashData = CryptoUtils.hash(data.getBytes()).getHash();
 
-            if(request.getMethod().equals("GET")) {
-                if (!data.equals("")) {
-                    http.parseResponse(
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: html\r\n" +
-                                    "Content-Length: " + body.length() + "\r\n" +
-                                    "Server: localhost\r\n" +
-                                    "\r\n" +
-                                    body).writeTo(client.getOutputStream());
-                } else {
-                    http.parseResponse("HTTP/1.1 404 Not Found\n" +
-                            "Content-Type: text/plain\n" +
-                            "Content-Length: 0\n" +
-                            "\n").writeTo(client.getOutputStream());
+                String hash = new String(hashData);
+
+                //System.out.println(takeHashUri(request));
+
+
+
+                String body="<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<body>\n" +
+                        "\n" +
+                        "<h1>Text to Base64</h1>\n" +
+                        "<p>"+hash+"</p>\n" +
+                        "\n" +
+                        "</body>\n" +
+                        "</html>";
+
+                if(request.getMethod().equals("GET")) {
+                    if (!data.equals("")) {
+                        http.parseResponse(
+                                "HTTP/1.1 200 OK\r\n" +
+                                        "Content-Type: html\r\n" +
+                                        "Content-Length: " + body.length() + "\r\n" +
+                                        "Server: localhost\r\n" +
+                                        "\r\n" +
+                                        body).writeTo(client.getOutputStream());
+                    } else {
+                        http.parseResponse("HTTP/1.1 404 Not Found\n" +
+                                "Content-Type: text/plain\n" +
+                                "Content-Length: 0\n" +
+                                "\n").writeTo(client.getOutputStream());
+                    }
+                }else {
+                    //idk
                 }
-                if (query.length==0) {
-                    throw new RemoteCryptoArrayException(query, http, client.getOutputStream());
-                }
-            }else {
-                //idk
+            } catch (Exception e) {
+                String body="<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "<body>\n" +
+                        "\n" +
+                        "<h1>Text to Base64</h1>\n" +
+                        "<p>"+"HELLOW WORLD"+"</p>\n" +
+                        "\n" +
+                        "</body>\n" +
+                        "</html>";
+
+                http.parseResponse(
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: html\r\n" +
+                                "Content-Length: " + body.length() + "\r\n" +
+                                "Server: localhost\r\n" +
+                                "\r\n" +
+                                body).writeTo(client.getOutputStream());
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-        catch (RemoteCryptoArrayException e) {
-            e.printStackTrace();
-        }
     }
+
+    private boolean takeHashUri(RawHttpRequest request) {
+        String s = request.getUri().toString();
+        var sa = s.split("/");
+
+        var hs = sa[3];
+
+        var h = hs.split("data");
+
+        if(h[0].equals("hash?"))
+            return true;
+        else
+            return false;
+    }
+
 }
