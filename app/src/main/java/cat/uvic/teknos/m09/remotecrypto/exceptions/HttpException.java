4e0ef98c36166d0b918be76ad670e3a1d3d5112a
Miquel Gual Pascual <miquel.gual.pascual@uvic.cat>
2022-12-15 22:45:53 +0100

    Added the exceptions

---
+++ app/src/main/java/cat/uvic/teknos/m09/remotecrypto/exceptions/HttpException.java
@@ -0,0 +1,8 @@
+package cat.uvic.teknos.m09.remotecrypto.exceptions;
+
+public class HttpException extends Exception{
+
+    public HttpException(){
+        super("Error on Http Server exception");
+    }
+}
