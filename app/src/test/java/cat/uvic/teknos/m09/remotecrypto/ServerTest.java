d40dc669ef1edc80d2429a81b3d52c88ea0d987b
Miquel Gual Pascual <miquel.gual.pascual@uvic.cat>
2022-12-15 23:18:56 +0100

    Added the Http Server Test

--- app/src/test/java/cat/uvic/teknos/m09/remotecrypto/ServerTest.java
+++ app/src/test/java/cat/uvic/teknos/m09/remotecrypto/ServerTest.java
@@ -3,12 +3,44 @@
  */
 package cat.uvic.teknos.m09.remotecrypto;
 
+import cat.uvic.teknos.m09.remotecrypto.exceptions.*;
+import cat.uvic.teknos.m09.remotecrypto.servers.ServerHttp;
+import cat.uvic.teknos.m09.remotecrypto.servers.threads.ServerHttpThread;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import rawhttp.core.RawHttp;
+import rawhttp.core.RawHttpRequest;
+import rawhttp.core.RawHttpResponse;
 
-class ServerTest {
-    @Test void appHasAGreeting() {
-        Server classUnderTest = new Server();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+import java.io.IOException;
+import java.net.Socket;
+
+public class ServerTest {
+    private static ServerHttpThread http;
+    @BeforeAll
+    static void init(){
+        http = new ServerHttpThread();
+    }
+    @Test
+    void throwNotFoundException(){
+        Assertions.assertThrows(NotFoundException.class,()->http.controller("randompath",""));
+    }
+
+    @Test
+    void successfullResponse() throws NotFoundException, InternalServerErrorException, BadRequestException, HttpException {
+        String response = http.controller("/cryptoutils/hash","text=ABC");
+        Assertions.assertFalse(response.isEmpty());
+    }
+
+    @Test
+    void badRequestException(){
+        Assertions.assertThrows(BadRequestException.class,()->http.controller("/cryptoutils/hash",""));
+    }
+
+    @Test
+    void connectionException(){
+        Assertions.assertThrows(ConnectionException.class,()->http.initConnection());
     }
 }
