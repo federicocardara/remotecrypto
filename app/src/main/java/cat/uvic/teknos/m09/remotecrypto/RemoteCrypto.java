00f5ecd2564353d81206f9c521e2ccf8087a6083
Ferran Vancells Borralleras <ferran.vancells@uvic.cat>
2022-12-15 18:33:42 +0100

    Create FTPConnection

--- app/src/main/java/cat/uvic/teknos/m09/remotecrypto/RemoteCrypto.java
+++ app/src/main/java/cat/uvic/teknos/m09/remotecrypto/RemoteCrypto.java
@@ -1,6 +1,7 @@
 package cat.uvic.teknos.m09.remotecrypto;
 
 import cat.uvic.teknos.m09.remotecrypto.ConnectionsViaTelnet.ServerTelnet;
+import cat.uvic.teknos.m09.remotecrypto.FTP.FTPConnection;
 import cat.uvic.teknos.m09.remotecrypto.HTTP.ServerHttp;
 
 import java.io.IOException;
@@ -9,12 +10,16 @@ public class RemoteCrypto {
     private static ServerHttp serverHttp;
     private static ServerTelnet serverTelnet;
 
+    private static FTPConnection ftpConnection;
+
     public static void main(String ...args) throws IOException {
         serverHttp = new ServerHttp();
         serverTelnet = new ServerTelnet();
+        ftpConnection = new FTPConnection();
     }
-    public static void joinServers(){
+    public static void joinServers() throws InterruptedException {
         serverTelnet.joinThread();
         serverHttp.joinThread();
+        ftpConnection.join();
     }
 }
