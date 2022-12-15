8147ff53ab555a24de78c386a5738566ecfa4f73
karim <abdelkarim.el@uvic.cat>
2022-12-15 16:11:35 +0100

    Ftp client connection: connection to ftp,get file of directory, calculate hash and create file .hash

--- app/src/main/java/cat/uvic/teknos/m09/remotecrypto/clients/FTPCli.java
+++ app/src/main/java/cat/uvic/teknos/m09/remotecrypto/clients/FTPCli.java
@@ -1,12 +1,15 @@
 package cat.uvic.teknos.m09.remotecrypto.clients;
 
+import cat.uvic.teknos.m09.elbouzzaouiabdelkarim.cryptoutils.CryptoUtils;
+import cat.uvic.teknos.m09.elbouzzaouiabdelkarim.cryptoutils.Exceptions.MissingPropertiesException;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.security.NoSuchAlgorithmException;
 
 public class FTPCli {
-
+        static CryptoUtils cr = new CryptoUtils();
     public static void main(String[] args) {
         try{
             run();
@@ -15,14 +18,24 @@ public class FTPCli {
         }
     }
 
-    private static void run() throws IOException {
+    private static void run() throws IOException, MissingPropertiesException, NoSuchAlgorithmException {
         var ftpClient = new FTPClient();
         ftpClient.connect("localhost");
 
         ftpClient.login("admin","admin1234");
         ftpClient.changeWorkingDirectory("ftp");
+        var f = ftpClient.listFiles("ftp");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        for (FTPFile file: f){
+            if(file.isFile()){
+                ftpClient.retrieveFile(file.getName(),out);
+                var encdrypted = cr.hash(out.toByteArray());
+                ftpClient.storeFile(file.getName()+".hash",new ByteArrayInputStream((encdrypted.getHash()+"\n"+encdrypted.getAlgorithm()+"\n"+encdrypted.getSalt()).getBytes()));
+            }
+        }
         ftpClient.storeFile("test3.txt",FTPCli.class.getResourceAsStream("/test6.txt"));
-        ftpClient.storeFile("test4.txt",new ByteArrayInputStream("test4".getBytes()));
 
         if(ftpClient.listFiles("test3.txt").length!=0){
             System.out.println("OKAY");
