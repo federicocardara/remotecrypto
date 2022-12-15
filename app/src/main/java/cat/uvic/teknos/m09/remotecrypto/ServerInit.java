8a6c4f31ad75b55eaa3983293a571c85caaf070b
karim <abdelkarim.el@uvic.cat>
2022-12-15 20:37:52 +0100

    implement ftpConnection on ServerInit

--- app/src/main/java/cat/uvic/teknos/m09/remotecrypto/ServerInit.java
+++ app/src/main/java/cat/uvic/teknos/m09/remotecrypto/ServerInit.java
@@ -2,15 +2,19 @@ package cat.uvic.teknos.m09.remotecrypto;
 
 import cat.uvic.teknos.m09.remotecrypto.servers.ServerHttp;
 import cat.uvic.teknos.m09.remotecrypto.servers.ServerTelnet;
+import cat.uvic.teknos.m09.remotecrypto.servers.thread.FTPConnection;
 
 public class ServerInit {
     private static ServerHttp http;
     private static ServerTelnet telnet;
+    private static FTPConnection ftpConnection;
 
     public static void main(String ...args){
         try{
             http = new ServerHttp();
             telnet = new ServerTelnet();
+            ftpConnection = new FTPConnection();
+            ftpConnection.init();
         }catch(Exception e){
             e.printStackTrace();
         }
