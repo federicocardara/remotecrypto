baba55c5237407465f70e5d7d3062b6b0a105111
xperalta <xainbusiness@gmail.com>
2023-01-03 23:53:54 +0100

    Resuletos problemas relacionados con la conexion de la terminal y tambien la excepcion, Rem ha sido editada

--- app/src/main/java/cat/uvic/teknos/m09/remotecrypto/exceptions/serverSocketException.java
+++
@@ -1,7 +0,0 @@
-package cat.uvic.teknos.m09.remotecrypto.exceptions;
-
-public class serverSocketException {
-    public void ServerSocketException() {
-        System.out.println("ERROR: CAN'T CREATE SERVER SOCKET");
-    }
-}
